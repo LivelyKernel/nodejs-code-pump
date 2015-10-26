b242e3aab51942a13e1cf9ce80215777a87c48c9
Robert Krahn <robert.krahn@gmail.com>
2015-10-25 17:50:57 -0700

    index for require

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = {
-  process: require("./lib/process.js")
+/*global process, require*/
+
+var evaluator = require("./lib/evaluator");
+var server = require("./lib/server");
+
+function start(host, port, thenDo) {
+  port = port || 9010;
+  host = host || "0.0.0.0";
+  evaluator.wrapModuleLoad()
+  server.startServer({host: host, port: port}, (err, msger) => {
+    err && console.error(err);
+    msger && process.on("close", () => msger.close());
+    thenDo && thenDo(err, msger);
+  });
 }
+
+module.exports = start;
