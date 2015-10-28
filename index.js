68831786cfb10c3ea2517df91592a89ae4e567ad
Robert Krahn <robert.krahn@gmail.com>
2015-10-27 18:00:23 -0700

    proper start interface for use from nodejs project

--- index.js
+++ index.js
@@ -2,16 +2,32 @@
 
 var evaluator = require("./lib/evaluator");
 var server = require("./lib/server");
+var messenger;
 
 function start(host, port, thenDo) {
+  if (messenger && messenger.serverState)
+    return thenDo && thenDo(null, messenger);
   port = port || 9010;
   host = host || "0.0.0.0";
   evaluator.wrapModuleLoad()
   server.startServer({host: host, port: port}, (err, msger) => {
     err && console.error(err);
+    messenger = msger;
     msger && process.on("close", () => msger.close());
     thenDo && thenDo(err, msger);
   });
 }
 
-module.exports = start;
+function stop(thenDo) {
+  if (!messenger || !messenger.serverState) return thenDo();
+  server.closeServer(messenger.serverState, err => {
+    messenger = null;
+    thenDo && thenDo(err);
+  });
+}
+
+module.exports = {
+  start: start,
+  stop: stop,
+  get messenger() { return messenger; }
+}
